b0ebe765479715f5c59743fffa110511a316426f
capricorn99 <darlinreadback@gmail.com>
2021-03-15 08:44:32 +0700

    a

--- stm32f4xx_drivers/Debug/Src/006spi_tx_testing.d
+++ stm32f4xx_drivers/Debug/Src/006spi_tx_testing.d
@@ -1,1 +1,13 @@
-Src/006spi_tx_testing.o: ../Src/006spi_tx_testing.c
+Src/006spi_tx_testing.o: ../Src/006spi_tx_testing.c \
+ E:/Knowledge/Stm32f407xx\ Discovery/workspace/stm32f4xx_drivers/drivers/Inc/stm32f4xx.h \
+ E:/Knowledge/Stm32f407xx\ Discovery/workspace/stm32f4xx_drivers/drivers/Inc/stm32f4xx_gpio_driver.h \
+ E:/Knowledge/Stm32f407xx\ Discovery/workspace/stm32f4xx_drivers/drivers/Inc/stm32f4xx.h \
+ E:/Knowledge/Stm32f407xx\ Discovery/workspace/stm32f4xx_drivers/drivers/Inc/stm32f4xx_spi_driver.h
+
+E:/Knowledge/Stm32f407xx\ Discovery/workspace/stm32f4xx_drivers/drivers/Inc/stm32f4xx.h:
+
+E:/Knowledge/Stm32f407xx\ Discovery/workspace/stm32f4xx_drivers/drivers/Inc/stm32f4xx_gpio_driver.h:
+
+E:/Knowledge/Stm32f407xx\ Discovery/workspace/stm32f4xx_drivers/drivers/Inc/stm32f4xx.h:
+
+E:/Knowledge/Stm32f407xx\ Discovery/workspace/stm32f4xx_drivers/drivers/Inc/stm32f4xx_spi_driver.h:
