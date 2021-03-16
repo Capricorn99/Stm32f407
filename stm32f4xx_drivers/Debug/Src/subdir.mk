edc2af9fc78fbd8ca86d4280fd006f1ae7683761
capricorn99 <darlinreadback@gmail.com>
2021-03-16 21:23:38 +0700

    a

--- stm32f4xx_drivers/Debug/Src/subdir.mk
+++ stm32f4xx_drivers/Debug/Src/subdir.mk
@@ -4,26 +4,16 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Src/007spi_rxonly_arduino.c \
-../Src/syscalls.c \
-../Src/sysmem.c 
+../Src/007spi_rxonly_arduino.c 
 
 OBJS += \
-./Src/007spi_rxonly_arduino.o \
-./Src/syscalls.o \
-./Src/sysmem.o 
+./Src/007spi_rxonly_arduino.o 
 
 C_DEPS += \
-./Src/007spi_rxonly_arduino.d \
-./Src/syscalls.d \
-./Src/sysmem.d 
+./Src/007spi_rxonly_arduino.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 Src/007spi_rxonly_arduino.o: ../Src/007spi_rxonly_arduino.c
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F407G_DISC1 -DSTM32F4 -DSTM32F407VGTx -DDEBUG -c -I"E:/Knowledge/Stm32f407xx Discovery/workspace/stm32f4xx_drivers/drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/007spi_rxonly_arduino.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
-Src/syscalls.o: ../Src/syscalls.c
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F407G_DISC1 -DSTM32F4 -DSTM32F407VGTx -DDEBUG -c -I"E:/Knowledge/Stm32f407xx Discovery/workspace/stm32f4xx_drivers/drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/syscalls.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
-Src/sysmem.o: ../Src/sysmem.c
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F407G_DISC1 -DSTM32F4 -DSTM32F407VGTx -DDEBUG -c -I"E:/Knowledge/Stm32f407xx Discovery/workspace/stm32f4xx_drivers/drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/sysmem.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 
